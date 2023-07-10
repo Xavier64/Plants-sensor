561fffadc92f2eaf02353722b8e18111189cc335
Xavier64 <xavierpincon@gmail.com>
2023-07-10 16:25:04 +0200

    cards and historic

--- src/app/intranet/historic/historic.component.ts
+++ src/app/intranet/historic/historic.component.ts
@@ -1,4 +1,4 @@
-import { Component,OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HistoricI } from '../utils/modeles/Types';
 import { HistoricService } from './historic.service';
 import { Chart } from 'chart.js/auto';
@@ -6,12 +6,12 @@ import { Chart } from 'chart.js/auto';
 @Component({
   selector: 'app-historic',
   templateUrl: './historic.component.html',
-  styleUrls: ['./historic.component.scss']
+  styleUrls: ['./historic.component.scss'],
 })
 export class HistoricComponent implements OnInit {
   histo: HistoricI[] = [];
 
-  constructor(public historicService:HistoricService) { }
+  constructor(public historicService: HistoricService) {}
 
   async ngOnInit(): Promise<void> {
     this.fetchHistoric();
@@ -26,66 +26,68 @@ export class HistoricComponent implements OnInit {
         id: item['id'],
         created_at: item['created_at'],
         humidity: item['humidity'],
-        macAddress: item['macAddress']
+        macAddress: item['macAddress'],
       }));
-        console.log(this.histo.map((item) => item['humidity']));
-        this.renderChart();
+      console.log(this.histo.map((item) => item['humidity']));
+      this.renderChart();
     }
     if (error) {
-      console.log("Error" , error);      
+      console.log('Error', error);
     }
   }
 
   renderChart() {
-    const myChart = new Chart("bar", {
+    const myChart = new Chart('bar', {
       type: 'bar',
-      data: { 
-        labels: this.histo.map((item) => { const date = new Date(item.created_at); // Valeurs dynamiques
-          return date.toLocaleString('fr-FR', { // pour formater la date au format Français
+      data: {
+        labels: this.histo.map((item) => {
+          const date = new Date(item.created_at); // Valeurs dynamiques
+          return date.toLocaleString('fr-FR', {
+            // pour formater la date au format Français
             year: 'numeric',
             month: '2-digit',
             day: '2-digit',
             hour: '2-digit',
             minute: '2-digit',
-            second: '2-digit'
+            second: '2-digit',
           });
-        }),              
-        datasets: [{
-          label: 'Humidité',
-          data: this.histo.map((item) => item['humidity']), 
-          /* [100, 80, 65, 50, 40, 35, 20], */
-          borderWidth: 4,  
-          borderColor: "#1e3860",
-          backgroundColor: "#1f375b",                                   
-        }],        
+        }),
+        datasets: [
+          {
+            label: 'Humidity',
+            data: this.histo.map((item) => item['humidity']),
+            /* [100, 80, 65, 50, 40, 35, 20], */
+            borderWidth: 4,
+            borderColor: '#1e3860',
+            backgroundColor: '#1f375b',
+          },
+        ],
       },
       options: {
-        responsive: true, 
-        maintainAspectRatio: false, 
-        scales: {          
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
           y: {
             beginAtZero: true,
-            ticks: { 
+            ticks: {
               color: '#ffffff',
-              //stepSize: 20,              
+              //stepSize: 20,
             },
             grid: {
-              color: "#4ECB71" 
-            }
+              color: '#4ECB71',
+            },
           },
           x: {
-            max:12,
-            ticks: { 
-              color: '#ffffff'
+            max: 12,
+            ticks: {
+              color: '#ffffff',
             },
             grid: {
-              color: "#4ECB71" 
-            }
-          }
-        }
-      }
+              color: '#4ECB71',
+            },
+          },
+        },
+      },
     });
   }
-
 }
-
